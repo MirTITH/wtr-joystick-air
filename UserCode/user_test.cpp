e302b8ecc5b3300bbc188e76606cbe7f66843469
TITH <1023515576@qq.com>
2023-05-27 21:29:17 +0800

    aaa

--- UserCode/user_test.cpp
+++ UserCode/user_test.cpp
@@ -16,6 +16,7 @@
 #include "Encoder/encoder_define.hpp"
 #include "as69_config.hpp"
 #include "TouchScreen/GT911/gt911_define.hpp"
+#include "Button/buttons.h"
 
 #define Led_Pin  GPIO_PIN_1
 #define Led_Port GPIOA
@@ -28,9 +29,11 @@ void TestThreadEntry(void *argument)
 {
     (void)argument;
 
-    auto a_dashboard         = dashboard_mgr.NewDashboard(252, "TxRxBytes");
     auto joystickl_dashboard = dashboard_mgr.NewDashboard(250, "JoystickL");
     auto joystickr_dashboard = dashboard_mgr.NewDashboard(251, "JoystickR");
+    auto encoder_dashboard   = dashboard_mgr.NewDashboard(252, "Encoder");
+    auto button_dashboard    = dashboard_mgr.NewDashboard(253, "Buttons(0-10 bit)");
+    auto button2_dashboard   = dashboard_mgr.NewDashboard(254, "Buttons(11-21 bit)");
 
     stringstream sstr;
     sstr.precision(4);
@@ -38,23 +41,32 @@ void TestThreadEntry(void *argument)
 
     uint32_t PreviousWakeTime = xTaskGetTickCount();
 
-    extern volatile uint32_t MavTotalBytesSent;
-    extern volatile uint32_t MavTotalBytesGot;
+    // extern volatile uint32_t MavTotalBytesSent;
+    // extern volatile uint32_t MavTotalBytesGot;
 
     while (true) {
         // time_dashboard->SetMsg(xTaskGetTickCount() / 1000.0);
-        sstr.str("");
-        sstr << MavTotalBytesSent << "," << MavTotalBytesGot;
-        a_dashboard->SetMsg(sstr.str());
         sstr.str("");
         sstr << JoystickL.Pos().x << "," << JoystickL.Pos().y;
         joystickl_dashboard->SetMsg(sstr.str());
         sstr.str("");
         sstr << JoystickR.Pos().x << "," << JoystickR.Pos().y;
         joystickr_dashboard->SetMsg(sstr.str());
-        // sstr.str("");
-        // sstr << KnobEncoderR.Count() << "," << KnobEncoderL.Count();
-        // encoder_dashboard->SetMsg(sstr.str());
+        sstr.str("");
+        sstr << KnobEncoderL.Count() << "," << KnobEncoderR.Count();
+        encoder_dashboard->SetMsg(sstr.str());
+
+        sstr.str("");
+        for (size_t i = 1; i <= 11; i++) {
+            sstr << (int)Buttons_Read(i);
+        }
+        button_dashboard->SetMsg(sstr.str());
+
+        sstr.str("");
+        for (size_t i = 12; i <= 22; i++) {
+            sstr << (int)Buttons_Read(i);
+        }
+        button2_dashboard->SetMsg(sstr.str());
 
         if (TouchScreen.NumberOfTouchPoint() == 3) {
             if (xTaskGetTickCount() - last_3_points_time > 500) {
