011d85924638330556c14fbbeba2d52f2b68a475
Xie Yang <1023515576@qq.com>
2023-09-19 10:38:22 +0800

    9 axis fusion

--- UserCode/user_test.cpp
+++ UserCode/user_test.cpp
@@ -37,7 +37,7 @@ extern "C" {
 
 using namespace std;
 
-#define DEFAULT_MPU_HZ  (100) // 100Hz
+#define DEFAULT_MPU_HZ  (20)
 #define COMPASS_READ_MS (100)
 
 // 陀螺仪方向设置
@@ -82,6 +82,54 @@ uint8_t run_self_test(void)
         return 0;
     } else
         return 1;
+}
+
+static void tap_cb(unsigned char direction, unsigned char count)
+{
+    // switch (direction) {
+    //     case TAP_X_UP:
+    //         MPL_LOGI("Tap X+ ");
+    //         break;
+    //     case TAP_X_DOWN:
+    //         MPL_LOGI("Tap X- ");
+    //         break;
+    //     case TAP_Y_UP:
+    //         MPL_LOGI("Tap Y+ ");
+    //         break;
+    //     case TAP_Y_DOWN:
+    //         MPL_LOGI("Tap Y- ");
+    //         break;
+    //     case TAP_Z_UP:
+    //         MPL_LOGI("Tap Z+ ");
+    //         break;
+    //     case TAP_Z_DOWN:
+    //         MPL_LOGI("Tap Z- ");
+    //         break;
+    //     default:
+    //         return;
+    // }
+    // MPL_LOGI("x%d\n", count);
+    // return;
+}
+
+static void android_orient_cb(unsigned char orientation)
+{
+    // switch (orientation) {
+    //     case ANDROID_ORIENT_PORTRAIT:
+    //         MPL_LOGI("Portrait\n");
+    //         break;
+    //     case ANDROID_ORIENT_LANDSCAPE:
+    //         MPL_LOGI("Landscape\n");
+    //         break;
+    //     case ANDROID_ORIENT_REVERSE_PORTRAIT:
+    //         MPL_LOGI("Reverse Portrait\n");
+    //         break;
+    //     case ANDROID_ORIENT_REVERSE_LANDSCAPE:
+    //         MPL_LOGI("Reverse Landscape\n");
+    //         break;
+    //     default:
+    //         return;
+    // }
 }
 
 uint8_t MPU9250_MPL_Init(void)
@@ -130,24 +178,26 @@ uint8_t MPU9250_MPL_Init(void)
         inv_set_gyro_sample_rate(1000000L / gyro_rate);
         inv_set_accel_sample_rate(1000000L / gyro_rate);
         inv_set_compass_sample_rate(COMPASS_READ_MS * 1000L); // 宏定义在头文件COMPASS_READ_MS
-        inv_set_gyro_orientation_and_scale(
-            inv_orientation_matrix_to_scalar(gyro_orientation), (long)gyro_fsr << 15);
-        inv_set_accel_orientation_and_scale(
-            inv_orientation_matrix_to_scalar(gyro_orientation), (long)accel_fsr << 15);
-        inv_set_compass_orientation_and_scale(
-            inv_orientation_matrix_to_scalar(comp_orientation), (long)compass_fsr << 15);
+        inv_set_gyro_orientation_and_scale(inv_orientation_matrix_to_scalar(gyro_orientation), (long)gyro_fsr << 15);
+        inv_set_accel_orientation_and_scale(inv_orientation_matrix_to_scalar(gyro_orientation), (long)accel_fsr << 15);
+        inv_set_compass_orientation_and_scale(inv_orientation_matrix_to_scalar(comp_orientation), (long)compass_fsr << 15);
 
         res = dmp_load_motion_driver_firmware(); // 加载dmp固件
-        if (res)
-            return 6;
+        if (res) return 6;
         printf("DMP load OK!\n");
+
         res = dmp_set_orientation(inv_orientation_matrix_to_scalar(gyro_orientation)); // 设置陀螺仪方向
-        if (res)
-            return 7;
+        if (res) return 7;
         printf("DMP set orientation OK!\n");
-        res = dmp_enable_feature(DMP_FEATURE_6X_LP_QUAT | DMP_FEATURE_TAP | // 设置dmp功能
-                                 DMP_FEATURE_ANDROID_ORIENT | DMP_FEATURE_SEND_RAW_ACCEL | DMP_FEATURE_SEND_CAL_GYRO |
-                                 DMP_FEATURE_GYRO_CAL);
+
+        dmp_register_tap_cb(tap_cb);
+        dmp_register_android_orient_cb(android_orient_cb);
+
+        // res = dmp_enable_feature(DMP_FEATURE_6X_LP_QUAT | DMP_FEATURE_TAP |
+        //                          DMP_FEATURE_ANDROID_ORIENT | DMP_FEATURE_SEND_RAW_ACCEL | DMP_FEATURE_SEND_CAL_GYRO |
+        //                          DMP_FEATURE_GYRO_CAL);
+        res = dmp_enable_feature(DMP_FEATURE_6X_LP_QUAT | DMP_FEATURE_TAP |
+                                 DMP_FEATURE_SEND_RAW_ACCEL | DMP_FEATURE_SEND_CAL_GYRO | DMP_FEATURE_GYRO_CAL);
         if (res)
             return 8;
         printf("DMP feature set OK!\n");
@@ -155,10 +205,11 @@ uint8_t MPU9250_MPL_Init(void)
         if (res)
             return 9;
         printf("DMP output rate set OK!\n");
-        res = run_self_test(); // 自检
-        if (res)
-            return 10;
-        printf("self test OK!\n");
+
+        // res = run_self_test(); // 自检
+        // if (res) return 10;
+        // printf("self test OK!\n");
+
         res = mpu_set_dmp_state(1); // 使能DMP
         if (res)
             return 11;
@@ -206,6 +257,10 @@ uint8_t MPU9250_MPL_getData(float *pitch, float *roll, float *yaw)
         inv_build_accel(accel, 0, sensor_timestamp); // 把加速度值发给MPL
     }
 
+    if (sensors & INV_WXYZ_QUAT) {
+        inv_build_quat(quat, 0, sensor_timestamp);
+    }
+
     if (!mpu_get_compass_reg(compass_short, &sensor_timestamp)) {
         compass[0] = (long)compass_short[0];
         compass[1] = (long)compass_short[1];
@@ -219,6 +274,58 @@ uint8_t MPU9250_MPL_getData(float *pitch, float *roll, float *yaw)
     *pitch = -(data[1] / q16);
     *yaw   = -data[2] / q16;
     return 0;
+}
+
+void MpuLoop()
+{
+    unsigned long sensor_timestamp;
+    inv_time_t timestamp;
+    short gyro[3], accel_short[3], compass_short[3], sensors;
+    unsigned char more;
+    long compass[3], accel[3], quat[4], temperature;
+    long data[9];
+    int8_t accuracy;
+
+    // q30，q16格式,long转float时的除数.
+    // static const float q30 = 1073741824.0f;
+    static const float q16 = 65536.0f;
+
+    if (dmp_read_fifo(gyro, accel_short, quat, &sensor_timestamp, &sensors, &more)) {
+        return;
+    }
+
+    if (sensors & INV_XYZ_GYRO) {
+        inv_build_gyro(gyro, sensor_timestamp); // 把新数据发送给MPL
+        mpu_get_temperature(&temperature, &sensor_timestamp);
+        inv_build_temp(temperature, sensor_timestamp); // 把温度值发给MPL，只有陀螺仪需要温度值
+    }
+
+    if (sensors & INV_XYZ_ACCEL) {
+        accel[0] = (long)accel_short[0];
+        accel[1] = (long)accel_short[1];
+        accel[2] = (long)accel_short[2];
+        inv_build_accel(accel, 0, sensor_timestamp); // 把加速度值发给MPL
+    }
+
+    if (sensors & INV_WXYZ_QUAT) {
+        inv_build_quat(quat, 0, sensor_timestamp);
+    }
+
+    if (!mpu_get_compass_reg(compass_short, &sensor_timestamp)) {
+        compass[0] = (long)compass_short[0];
+        compass[1] = (long)compass_short[1];
+        compass[2] = (long)compass_short[2];
+        inv_build_compass(compass, 0, sensor_timestamp); // 把磁力计值发给MPL
+    }
+    inv_execute_on_data();
+    inv_get_sensor_type_euler(data, &accuracy, &timestamp);
+    // inv_get_sensor_type_quat
+
+    float roll  = (data[0] / q16);
+    float pitch = -(data[1] / q16);
+    float yaw   = -data[2] / q16;
+
+    printf("%f,%f,%f,%d,%d\n", pitch, yaw, roll, accuracy, more);
 }
 
 void TestThreadEntry(void *argument)
@@ -239,11 +346,13 @@ void TestThreadEntry(void *argument)
     uint32_t PreviousWakeTime = xTaskGetTickCount();
 
     while (true) {
-        float pitch, roll, yaw;
-        if (!MPU9250_MPL_getData(&pitch, &roll, &yaw))
-            printf("%f,%f,%f\n", pitch, yaw, roll);
-        else
-            printf("error:cannot get data\n");
-        vTaskDelayUntil(&PreviousWakeTime, 10);
+        // float pitch, roll, yaw;
+        // if (!MPU9250_MPL_getData(&pitch, &roll, &yaw))
+        //     printf("%f,%f,%f\n", pitch, yaw, roll);
+        // else
+        //     printf("error:cannot get data\n");
+        MpuLoop();
+        vTaskDelay(10);
+        // vTaskDelayUntil(&PreviousWakeTime, 25);
     }
 }
